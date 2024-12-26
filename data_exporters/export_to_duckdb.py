e433347c4d573267d0efc9e9af835bb686650b8d
jospablo777 <jospablo777@gmail.com>
2024-12-25 21:37:18 -0600

    Advanced in the logic

--- data_exporters/export_to_duckdb.py
+++ data_exporters/export_to_duckdb.py
@@ -6,9 +6,50 @@ import duckdb
 if 'data_exporter' not in globals():
     from mage_ai.data_preparation.decorators import data_exporter
 
+create_table_query = """
+    CREATE TABLE IF NOT EXISTS iowa_liquor_sales (
+        invoice_line_no TEXT PRIMARY KEY NOT NULL,
+        date TIMESTAMP NOT NULL,
+        store TEXT,
+        name TEXT,
+        address TEXT,
+        city TEXT,
+        zipcode TEXT,
+        store_location TEXT,
+        county_number TEXT,
+        county TEXT,
+        category TEXT,
+        category_name TEXT,
+        vendor_no TEXT,
+        vendor_name TEXT,
+        itemno TEXT,
+        im_desc TEXT,
+        pack REAL,
+        bottle_volume_ml REAL,
+        state_bottle_cost REAL,
+        state_bottle_retail REAL,
+        sale_bottles REAL,
+        sale_dollars REAL,
+        sale_liters REAL,
+        sale_gallons REAL,
+        liquor_type TEXT,
+        is_premium BOOLEAN,
+        bottle_size TEXT,
+        gov_profit_margin REAL,
+        gov_retail_markup_percentage REAL,
+        price_per_liter REAL,
+        price_per_gallon REAL,
+        total_volume_ordered_L REAL,
+        volume_to_revenue_ratio REAL,
+        week_day INTEGER,
+        is_weekend BOOLEAN,
+        quarter INTEGER
+);
+"""
+
 
 @data_exporter
-def export_data(data, *args, **kwargs):
+def export_data(data, schema, *args, **kwargs):
     """
     Exports data to some source.
 
@@ -22,13 +63,18 @@ def export_data(data, *args, **kwargs):
     """
     # Create a connection to a file called 'data/iowa_liquor.duckdb'
     # If the file doesn't exist it should create it
-    con = duckdb.connect("data/iowa_liquor.duckdb")
+    conn = duckdb.connect("data/iowa_liquor.duckdb")
     # Create a table and load data into it
-    con.sql("CREATE TABLE IF NOT EXISTS iowa_liquor_sales AS SELECT * FROM data")
+    conn.sql(create_table_query)
+
+    # Insert Polars DataFrame into DuckDB
+    conn.execute("INSERT INTO iowa_liquor_sales SELECT * FROM data")
+
     #con.sql("INSERT INTO test VALUES (42)")
     # query the table
-    con.sql("SELECT date, liquor_type, is_premium, sale_bottles, sale_dollars FROM iowa_liquor_sales LIMIT 20").show()
+    conn.sql("SELECT date, liquor_type, is_premium, sale_bottles, sale_dollars FROM iowa_liquor_sales LIMIT 20").show()
     # explicitly close the connection
-    con.close()
+    conn.close()
+
 
 
